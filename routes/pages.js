75f66b3f5cfc3bc141c963f64c75d073623efa5c
chayisslay <114644239+chayisslay@users.noreply.github.com>
2023-08-12 21:36:10 -0700

    post viewer

--- routes/pages.js
+++ routes/pages.js
@@ -3,31 +3,23 @@ const router = express.Router()
 
 const xmltojson = require('xml-js')
 
-router.get('/home', function(req, res) {
-
-    const { nnid, password, token } = req.cookies
-
-    fetch(`http://localhost:80/v1/people/person?nnid=${nnid}`).then(response => response.text()).then((accountFetch) => {
-        const account = JSON.parse(accountFetch)
-
-        fetch('http://localhost:80/v1/communities/0/posts?limit=10').then(response => response.text()).then((postsFetch) => {
-
-        const xmlToJSON = xmltojson.xml2json(postsFetch, {ignoreDoctype : true})
-
-        const posts = JSON.parse(xmlToJSON)
-
-        console.log(posts.elements[0].elements[5].elements)
-        
-            res.render('pages/home', {
-                account : account,
-                posts : posts.elements[0].elements[5].elements
-            })
-        })
-    })
-})
+const xmlparser = require('fast-xml-parser')
 
 router.get('/post', function(req, res) {
     res.render('pages/post.ejs')
+})
+
+router.get('/posts/:community_id', (req, res) => {
+    const community_id = req.params.community_id
+    const parser = new xmlparser.XMLParser();
+
+    fetch(`http://localhost:80/v1/communities/${community_id}/posts`).then(response => response.text()).then(xmlResult => {
+        const xmlFinal = parser.parse(xmlResult)
+
+        res.render('pages/posts.ejs', {
+            data : xmlFinal.result.posts
+        })
+    })
 })
 
 module.exports = router
