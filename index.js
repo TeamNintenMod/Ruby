114a513d1e0412ad8666e1e9de3332df0f53999a
chayisslay <114644239+chayisslay@users.noreply.github.com>
2023-08-12 19:19:13 -0700

    a TON of progress

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ const mysql = require('mysql')
 const { port, dbPassword } = config;
 const path = require('path')
 
+const xmlparser = require('fast-xml-parser')
+
 const logger = require('./other/logger')
 
 const cookieparser = require('cookie-parser')
@@ -12,7 +14,9 @@ const cookieparser = require('cookie-parser')
 const accountRoute = require("./routes/api/account")
 const pagesRoute = require('./routes/pages')
 const communityRoute = require('./routes/api/communties')
-const endpointRoute = require('./routes/api/endpoint')
+const endpointRoute = require('./routes/api/endpoint');
+const xmljs= require('xml-js');
+const xml = require('xml')
 
 var app = express();
 
@@ -35,6 +39,22 @@ app.get('/signUp', function(req, res) {
 app.get('/signin', function(req, res) {
     console.log(req.ip)
     res.render('./pages/signs/signIn.ejs')
+})
+
+app.get('/', (req, res) => {
+    console.log(logger.Get(req.originalUrl))
+
+    const parser = new xmlparser.XMLParser();
+
+    fetch('http://192.168.0.210:80/v1/communities').then(response => response.text()).then((xmlResult) => {
+        const xmlFinal = parser.parse(xmlResult)
+
+        res.render('./pages/index.ejs', {
+            data : xmlFinal.result.communities
+        })
+    });
+
+    
 })
 
 app.listen(port, () => {
